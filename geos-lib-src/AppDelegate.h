99f17e112b8ec49dd082a0aba487007b60c9b739
陈波 <innerpeacer@gmail.com>
2015-01-04 14:15:05 +0800

    Initial Commit

---
+++ geos-lib-src/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  geos-lib-src
+//
+//  Created by innerpeacer on 15/1/4.
+//  Copyright (c) 2015年 innerpeacer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
